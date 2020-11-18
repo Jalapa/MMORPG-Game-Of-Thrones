7204955e44738cda63f5bd7e0e51f19642ddee24
Rodrigo Inoue <rodrigoinoue1122@gmail.com>
2020-11-18 19:17:12 -0300

    Validacao do cadastro

--- app/controllers/cadastro.js
+++ app/controllers/cadastro.js
@@ -1,3 +1,21 @@
 module.exports.cadastro = function(application, req, res) {
-    res.render('cadastro')
+    res.render('cadastro', { validacao: {}, dadosForm: {} })
+}
+
+module.exports.cadastrar = function(application, req, res) {
+    let dadosForm = req.body;
+
+    req.assert('nome', "Nome não pode ser vazio").notEmpty()
+    req.assert('usuario', "Usuário não pode ser vazio").notEmpty()
+    req.assert('senha', "Senha não pode ser vazio").notEmpty()
+    req.assert('casa', "Casa não pode ser vazio").notEmpty()
+
+    let errors = req.validationErrors()
+
+    if (errors) {
+        res.render('cadastro', { validacao: errors, dadosForm: dadosForm })
+        return
+    }
+
+    res.send('cadastro')
 }
